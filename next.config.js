25167815844b3b3d5314959a404ecb5de66f27a2
Clément <duvivier.clem@gmail.com>
2022-10-28 01:40:30 +0200

    :rocket: first commit and setup of project

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
+
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  experimental: {
+    appDir: true
+  },
+  
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
 
 module.exports = nextConfig
