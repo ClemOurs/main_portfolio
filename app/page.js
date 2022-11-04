58ed60fd26d31bf3a6c2a9c40c567145211c9109
Clément <duvivier.clem@gmail.com>
2022-11-04 13:56:45 +0100

    :sparkles: waiting page and other style changes

--- app/page.js
+++ app/page.js
@@ -1,3 +1,42 @@
+'use client';
+import Image from 'next/image';
+import MemojiPicWaitingPage from '../public/assets/memojis/memoji_1.png'
+import variables from '../styles/abstracts/_variables.module.scss'
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>;
+    return(
+        <section id='waitScreen' style={{backgroundColor : variables.blackColor}}>
+            <div className='inProgress'>
+                <Image 
+                    src={MemojiPicWaitingPage}
+                    alt="my avatar"
+                    width={200}
+                />
+                <h1><span className='gradientEffect'>in dev <br /> comeback soon.. </span></h1>
+                <div className="btns">
+                    <a  
+                        style={{
+                            backgroundColor : variables.primaryColor, 
+                            color : variables.whiteColor
+                        }} 
+                        href="https://www.linkedin.com/in/clementduvivier/" 
+                        target="_blank"
+                    >
+                        linkedin
+                    </a>
+                    <a  
+                        style={{
+                            backgroundColor : variables.primaryColor, 
+                            color : variables.whiteColor
+                        }} 
+                        href="https://www.github.com/ClemOurs" 
+                        target="_blank"
+                    >
+                        github
+                    </a>
+                </div>
+            </div>
+            <footer style={{color : variables.whiteColor}}>Copyright © 2022 Clément Duvivier All rights reserved.</footer>
+        </section>
+    );
 }
